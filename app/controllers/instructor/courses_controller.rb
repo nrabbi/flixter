aef958032a863662412a26b5a6cacc09bdb79d4d
Nazmul Rabbi <nrabbi@outlook.com>
2016-10-29 13:38:11 +0000

    fixed bugs

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,7 @@
 class Instructor::CoursesController < ApplicationController
+  
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -11,17 +13,26 @@ class Instructor::CoursesController < ApplicationController
       redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity
-    end  
+    end
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
-  end
+  end           
 
 end
