60d2aa17fa9c8aa35bdd8768f15cbd168d64a3d3
Nazmul Rabbi <nrabbi@outlook.com>
2016-11-05 04:23:06 +0000

    added lessons permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,14 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   end
 
   private
-
   helper_method :current_lesson
   
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course) && current_lesson.section.course.user != current_user      
+      redirect_to course_path(current_lesson.section.course), alert: 'Unauthorized Access! Enroll in Course to receive access.'
+    end
   end
 
 end
